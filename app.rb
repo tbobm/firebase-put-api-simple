b74fc98c5541c5dff224af1d552694d24f4db352
Theo Massard <massar_t@etna-alternance.net>
2017-07-14 15:21:31 +0200

    Removed unused routes, lint code

--- app.rb
+++ app.rb
@@ -6,34 +6,20 @@ require 'firebase'
 set :bind, '0.0.0.0'
 set :port, 8080
 
-
-base_uri = ENV["FIREBASE_URI"]
+base_uri = ENV['FIREBASE_URI']
 
 firebase = Firebase::Client.new(base_uri)
 
-
 get '/' do
-        'hello world'
-end
-
-get '/test' do
-        'this is a tost'
-end
-
-get '/fb' do
-        firebase.push("test/2", {:id => 1})
+  'hello world'
 end
 
 get '/analyze' do
-        res = Chronic.parse(params['sentence'])
-        if (res != nil)
-        	puts "users/" + params['id'] + "/conversations/" + params['convId'] + "/rdv/"
-        	firebase.push("users/" + params['id'] + "/conversations/" + params['convId'] + "/rdv/", {:date => res})
-        else
-        	puts 'error'
-        end
-end
-
-get '/info' do
-        ENV["HELLO"]
+  res = Chronic.parse(params['sentence'])
+  if !res.nil?
+    puts "users/#{params['id']}/conversations/#{params['convId']}/rdv/"
+    firebase.push("users/#{params['id']}/conversations/#{params['convId']}/rdv/", date: res)
+  else
+    puts 'error'
+  end
 end
